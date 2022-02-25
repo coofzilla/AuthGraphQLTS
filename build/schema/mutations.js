88c7522bcdc7982443ef8427f523146cf4e72759
coofzilla <jeric614@yahoo.com>
2022-02-25 13:53:37 +0900

    mutations f/login, logout, signup

--- build/schema/mutations.js
+++ build/schema/mutations.js
@@ -1,1 +1,34 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mutation = void 0;
+const graphql_1 = require("graphql");
+const user_type_1 = require("./types/user_type");
+exports.mutation = new graphql_1.GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        signup: {
+            type: user_type_1.UserType,
+            args: {
+                email: { type: graphql_1.GraphQLString },
+                password: { type: graphql_1.GraphQLString },
+            },
+            resolve(parentValue, { email, password }, req) { },
+        },
+        logout: {
+            type: user_type_1.UserType,
+            resolve(parentValue, args, req) {
+                const { user } = req;
+                req.logout();
+                return user;
+            },
+        },
+        login: {
+            type: user_type_1.UserType,
+            args: {
+                email: { type: graphql_1.GraphQLString },
+                password: { type: graphql_1.GraphQLString },
+            },
+            resolve(parentValue, { email, password }, req) { },
+        },
+    },
+});
---
+++ src/schema/mutations.ts
@@ -0,0 +1,32 @@
+import { GraphQLObjectType, GraphQLString } from "graphql";
+import { UserType } from "./types/user_type";
+
+export const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, { email, password }, req) {},
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
+      },
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, { email, password }, req) {},
+    },
+  },
+});
