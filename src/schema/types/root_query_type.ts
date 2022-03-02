68e58628c2f0159d56859f1a13df8a449ed5134e
coofzilla <jeric614@yahoo.com>
2022-03-02 17:42:19 +0900

    comments f/how authenticated user verified

--- src/schema/types/root_query_type.ts
+++ src/schema/types/root_query_type.ts
@@ -5,6 +5,7 @@ import { UserType } from "./user_type";
 export const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    //if user, authenticated, else null
     user: {
       type: UserType,
       resolve(parentValue, args, req) {
