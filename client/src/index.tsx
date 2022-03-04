1797a157e75413818b432ff70169eccafa1d298c
coofzilla <jeric614@yahoo.com>
2022-03-04 12:11:51 +0900

    loginform associated w/router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import * as ReactDOM from "react-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./components/App";
+import LoginForm from "./components/LoginForm";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -13,7 +14,9 @@ const Root = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}></Route>
+        <Route path="/" element={<App />}>
+          <Route path="login" element={<LoginForm />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
